e8189744c85eed12dd1e33fc6a6fb221cf205d0e
GHADY-MATTA <ghady5255@gmail.com>
2025-05-02 11:52:41 +0300

    docker logo fix

--- client/vite-project/src/pages/Home.jsx
+++ client/vite-project/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import logoIcon from '../assets/Tutoron-gpt-logo.png'; // Make sure the path matches your actual logo location
+import logoIcon from '../assets/tutoron-gpt-logo.png'; // Make sure the path matches your actual logo location
 import { Link } from 'react-router-dom';
 
 const TutoronGPT = () => {
