372eef2bb011349843d6eeaf126c7187fc24a780
GHADY-MATTA <ghady5255@gmail.com>
2025-05-04 19:50:22 +0300

    dist and api

--- client/vite-project/src/api/api.js
+++ client/vite-project/src/api/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://35.180.110.219/api", // 🔥 Hardcoded for testing
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true, // If you’re using cookies (e.g., Laravel Sanctum)
+  withCredentials: true,
 });
 
 export default API;
