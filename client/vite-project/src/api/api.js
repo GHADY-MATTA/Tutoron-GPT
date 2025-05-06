788f9c50f709e3ba9ab0c05932dce33b825de737
GHADY-MATTA <ghady5255@gmail.com>
2025-05-06 18:32:54 +0300

    updated api.js

--- client/vite-project/src/api/api.js
+++ client/vite-project/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "http://52.47.190.216:8000/api", //  Hardcoded for testing
+  baseURL: "http://backend:8000/api", //  Hardcoded for testing
   headers: {
     'Content-Type': 'application/json',
   },
