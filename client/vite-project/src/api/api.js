ec4ecedbd1495f8437d5ea7af217ecfc42ffe1b9
GHADY-MATTA <ghady5255@gmail.com>
2025-05-06 17:00:43 +0300

    fixed docker compose

--- client/vite-project/src/api/api.js
+++ client/vite-project/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "http://51.44.18.191:8000/api", // 🔥 Hardcoded for testing
+  baseURL: "http://51.44.18.191:8000/api", //  Hardcoded for testing
   headers: {
     'Content-Type': 'application/json',
   },
