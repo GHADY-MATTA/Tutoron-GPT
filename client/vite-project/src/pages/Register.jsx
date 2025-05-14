cbe6103ba250ac09680d0d32263a13f8eea6dcdd
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 16:57:32 +0300

    Create email input field in form

--- client/vite-project/src/pages/Register.jsx
+++ client/vite-project/src/pages/Register.jsx
@@ -70,3 +70,15 @@ const handleSubmit = async (e) => {
               className="w-full px-4 py-2 rounded-lg bg-[#f0f9fb] text-[#054652] border border-[#0C9DB1] focus:outline-none focus:ring-2 focus:ring-[#0C9DB1]"
             />
           </div>
+          <div>
+            <label className="block text-sm font-semibold mb-1">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              required
+              placeholder="you@example.com"
+              className="w-full px-4 py-2 rounded-lg bg-[#f0f9fb] text-[#054652] border border-[#0C9DB1] focus:outline-none focus:ring-2 focus:ring-[#0C9DB1]"
+            />
+          </div>
