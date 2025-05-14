6e25f9633b4c9479f1c712db2934c049e995827e
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 10:02:09 +0300

    serialization

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be  for serialization.
      *
      * @var list<string>
      */
