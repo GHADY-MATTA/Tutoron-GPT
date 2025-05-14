5a6844327a4ff617f436b9167096201bda30e753
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 10:07:26 +0300

    update AppServiceProvider

--- server/app/Providers/AppServiceProvider.php
+++ server/app/Providers/AppServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any  services.
+     * Register any application services.
      */
     public function register(): void
     {
