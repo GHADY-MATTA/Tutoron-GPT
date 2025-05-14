f68890ff28fbbcdc4c5df95f1e63311aa148ad59
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 10:29:58 +0300

    Configure stdout encoding

--- server/scripts/fetch_transcript.py
+++ server/scripts/fetch_transcript.py
@@ -3,3 +3,6 @@ import json
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
 from urllib.parse import parse_qs, urlparse
+
+# 🛠 Fix output encoding immediately
+sys.stdout.reconfigure(encoding='utf-8')
