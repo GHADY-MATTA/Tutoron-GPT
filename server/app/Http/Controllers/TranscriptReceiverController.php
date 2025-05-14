2571fae9f34b82e2ca00735d8e3f89172a9a30b0
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 10:36:03 +0300

    Log incoming transcript details

--- server/app/Http/Controllers/TranscriptReceiverController.php
+++ server/app/Http/Controllers/TranscriptReceiverController.php
@@ -15,3 +15,8 @@ $request->validate([
 'title' => 'required|string',
 'transcript_raw' => 'required|string',
 ]);
+        Log::info('📥 Transcript received from Node.js', [
+            'video_id' => $request->video_id,
+            'title' => $request->title,
+            'lines' => substr_count($request->transcript_raw, "\n")
+        ]);
