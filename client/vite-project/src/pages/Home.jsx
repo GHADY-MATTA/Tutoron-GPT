63f0d9fc9573499d76f6065a76748577f718c719
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 16:14:30 +0300

    Create navigation bar structure

--- client/vite-project/src/pages/Home.jsx
+++ client/vite-project/src/pages/Home.jsx
@@ -6,3 +6,6 @@ const TutoronGPT = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <div className="font-inter bg-[#e1e5f2] text-[#022b3a]">
+    <nav className="bg-white shadow-sm">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
