9bc4a1ed5c76e964681157fd788ffac0ad2ecf63
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 10:01:52 +0300

    fixed attributes that are mass

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The  assignable.
+     * The attributes that are mass assignable.
      *
      * @var list<string>
      */
