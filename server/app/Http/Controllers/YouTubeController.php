8c2950709f6ddc7eb6531b03215156e1ff46bec6
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 10:40:02 +0300

    Create YouTubeController class

---
+++ server/app/Http/Controllers/YouTubeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class YouTubeController extends Controller
+{
+    public function store(Request $request)
+    {
