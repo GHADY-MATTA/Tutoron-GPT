585b5966706f5d8a794732c7a1d50c5596f05be5
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 10:56:42 +0300

    Define the TranscriptSummarizer class

---
+++ server/app/Services/TranscriptSummarizer.php
@@ -0,0 +1,9 @@
+namespace App\Services;
+
+use Prism\Prism\Prism;
+use Prism\Prism\Enums\Provider;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+class TranscriptSummarizer
+{
