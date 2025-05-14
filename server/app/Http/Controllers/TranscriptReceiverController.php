e4b78df8747100800d0a74af8677cf4b4557b4fa
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 10:35:19 +0300

    Added the TranscriptReceiverController

---
+++ server/app/Http/Controllers/TranscriptReceiverController.php
@@ -0,0 +1,10 @@
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Services\TranscriptSummarizer;
+
+class TranscriptReceiverController extends Controller
+{
+public function receive(Request $request, TranscriptSummarizer $summarizer)
+{
