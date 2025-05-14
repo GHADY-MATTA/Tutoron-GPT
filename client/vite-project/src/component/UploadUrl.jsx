2a58fb4da6a6dba15673aa6fade0874388f56455
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 15:59:32 +0300

    Set up initial imports and state

---
+++ client/vite-project/src/component/UploadUrl.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useVideo } from '../context/VideoContext';
+
+function extractVideoId(url) {
+  const match = url.match(/(?:v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
+  return match ? match[1] : null;
+}
+
+function UploadUrl() {
+  const [url, setUrl] = useState('');
+  const { setVideoId } = useVideo();
+  const [localVideoId, setLocalVideoId] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
