cd42dba71c05300d2bc017776e6c22ea944c2966
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 16:14:01 +0300

    Set up initial imports and component structure

---
+++ client/vite-project/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+import logoIcon from '../assets/tutoron-gpt-logo.png'; 
+import { Link } from 'react-router-dom';
+
+const TutoronGPT = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
