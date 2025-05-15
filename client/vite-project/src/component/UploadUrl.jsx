a5486ec904abf3e35bbb68ee4edb04ffa0d9c257
GHADY-MATTA <ghady5255@gmail.com>
2025-05-15 14:23:27 +0300

    Add state variables for URL, videoId, loading, success, and error

--- client/vite-project/src/component/UploadUrl.jsx
+++ client/vite-project/src/component/UploadUrl.jsx
@@ -5,3 +5,9 @@ import { useVideo } from '../context/VideoContext';
 function UploadUrl() {
     // Component logic will be added here
 }
+const [url, setUrl] = useState('');
+const { setVideoId } = useVideo();
+const [localVideoId, setLocalVideoId] = useState('');
+const [loading, setLoading] = useState(false);
+const [successMessage, setSuccessMessage] = useState('');
+const [errorMessage, setErrorMessage] = useState('');
