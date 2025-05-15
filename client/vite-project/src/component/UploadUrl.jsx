f4e8dcdba6f0e03592bca8693ee4b08d8a88e5c3
GHADY-MATTA <ghady5255@gmail.com>
2025-05-15 14:23:19 +0300

    Initial commit: Create UploadUrl component

---
+++ client/vite-project/src/component/UploadUrl.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useVideo } from '../context/VideoContext';
+
+function UploadUrl() {
+    // Component logic will be added here
+}
