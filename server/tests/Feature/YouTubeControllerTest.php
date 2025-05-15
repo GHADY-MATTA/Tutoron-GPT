fa8dfee22400f9c30c3d90120c8f8cafbd32bdd0
GHADY-MATTA <ghady5255@gmail.com>
2025-05-15 14:13:24 +0300

    Assert success status and response message

--- server/tests/Feature/YouTubeControllerTest.php
+++ server/tests/Feature/YouTubeControllerTest.php
@@ -11,7 +11,12 @@ public function it_forwards_youtube_url_and_returns_success_response()
         $response = $this->postJson('/api/youtube-transcript', [
             'url' => 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
         ]);
-    }
+    }$response->assertStatus(200)
+    ->assertJson([
+        'status' => 'ok',
+        'message' => 'YouTube URL successfully forwarded to local service. Processing will continue in the background.'
+    ]);
+
 
 // Test methods will be added here
 }
