b37eb2d178f71eaef45fb15001dd32dd16cf9882
GHADY-MATTA <ghady5255@gmail.com>
2025-05-15 14:13:15 +0300

    Send post request to YouTube URL endpoint

--- server/tests/Feature/YouTubeControllerTest.php
+++ server/tests/Feature/YouTubeControllerTest.php
@@ -7,8 +7,11 @@ class YouTubeControllerTest extends TestCase
 {/** @test */
 public function it_forwards_youtube_url_and_returns_success_response()
 {
-    // Test logic for forwarding YouTube URL
-}
+        // Test logic for forwarding YouTube URL
+        $response = $this->postJson('/api/youtube-transcript', [
+            'url' => 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        ]);
+    }
 
 // Test methods will be added here
 }
