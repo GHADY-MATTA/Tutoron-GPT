6ca60a36f2894e7d0343a76421d410bb959f0d38
GHADY-MATTA <ghady5255@gmail.com>
2025-05-15 14:33:44 +0300

    Create TutoronGPT component

---
+++ client/vite-project/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import logoIcon from '../assets/tutoron-gpt-logo.png'; // Make sure the path matches your actual logo location
+import { Link } from 'react-router-dom';
+
+const TutoronGPT = () => {
+  // Component logic will be added here
+};
