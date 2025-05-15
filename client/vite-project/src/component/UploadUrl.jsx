32df2c2664b9c4d659f129089690b02b8b6ac389
GHADY-MATTA <ghady5255@gmail.com>
2025-05-15 14:23:38 +0300

    Create extractVideoId function to extract video ID from YouTube URL

--- client/vite-project/src/component/UploadUrl.jsx
+++ client/vite-project/src/component/UploadUrl.jsx
@@ -11,3 +11,7 @@ const [localVideoId, setLocalVideoId] = useState('');
 const [loading, setLoading] = useState(false);
 const [successMessage, setSuccessMessage] = useState('');
 const [errorMessage, setErrorMessage] = useState('');
+function extractVideoId(url) {
+  const match = url.match(/(?:v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
+  return match ? match[1] : null;
+}
