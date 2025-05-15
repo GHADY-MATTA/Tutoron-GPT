084c88ac54e4631086f0acb1f31f7e8d10e3bf7e
GHADY-MATTA <ghady5255@gmail.com>
2025-05-15 14:13:04 +0300

    Test forwarding of YouTube URL

--- server/tests/Feature/YouTubeControllerTest.php
+++ server/tests/Feature/YouTubeControllerTest.php
@@ -4,6 +4,11 @@ namespace Tests\Feature;
 use Tests\TestCase;
 
 class YouTubeControllerTest extends TestCase
+{/** @test */
+public function it_forwards_youtube_url_and_returns_success_response()
 {
+    // Test logic for forwarding YouTube URL
+}
+
 // Test methods will be added here
 }
