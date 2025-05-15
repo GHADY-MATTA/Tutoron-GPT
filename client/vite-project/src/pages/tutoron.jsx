aca43d57ec44bcfced6cf401ae0ab0c8e5fb4221
GHADY-MATTA <ghady5255@gmail.com>
2025-05-15 14:39:23 +0300

    Create Tutoron component

---
+++ client/vite-project/src/pages/tutoron.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import logoIcon from '../assets/tutoron-gpt-logo.png'; // Adjust if your logo path is different
+import { FaRobot, FaUser, FaCog, FaSearch, FaPlus } from 'react-icons/fa';
+
+const Tutoron = () => {
+  // Component logic will be added here
+};
