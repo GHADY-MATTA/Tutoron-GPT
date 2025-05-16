bff64e958ec5ee5d56981d0f636afbd998802c7b
GHADY-MATTA <ghady5255@gmail.com>
2025-05-16 16:59:24 +0300

    upadted logs middleware

--- server/app/Http/Middleware/LogAllRequests.php
+++ server/app/Http/Middleware/LogAllRequests.php
@@ -5,16 +5,21 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Models\RequestLog;
 
 class LogAllRequests
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
+        RequestLog::create([
+            'method' => $request->method(),
+            'url' => $request->fullUrl(),
+            'ip' => $request->ip(),
+            'user_id' => optional(auth()->user())->id,
+            'controller' => optional($request->route())->getActionName(),
+            'payload' => json_encode($request->all())
+        ]);
+
         return $next($request);
     }
 }
