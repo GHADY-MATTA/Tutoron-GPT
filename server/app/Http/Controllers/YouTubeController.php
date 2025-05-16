2a2f5894c6008cd2e6fa095e2562b2915b54961d
GHADY-MATTA <ghady5255@gmail.com>
2025-05-16 18:53:20 +0300

    updated ngrok

--- server/app/Http/Controllers/YouTubeController.php
+++ server/app/Http/Controllers/YouTubeController.php
@@ -19,7 +19,7 @@ class YouTubeController extends Controller
 
         try {
             // Replace with your actual ngrok URL
-            $ngrokUrl = 'https://84a4-185-84-106-202.ngrok-free.app/receive';
+            $ngrokUrl = 'https://31b1-185-84-106-202.ngrok-free.app/receive';
 
             // ✅ Fire and forget: don't wait for Node to finish AI
             Http::timeout(seconds: 5)->post($ngrokUrl, [
