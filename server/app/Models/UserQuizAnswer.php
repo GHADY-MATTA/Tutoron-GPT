429166dde7e20e5eb4271e71b9cfb03251e12de2
GHADY-MATTA <ghady5255@gmail.com>
2025-05-17 15:14:21 +0300

    Inside UserQuizAnswer.php

--- server/app/Models/UserQuizAnswer.php
+++ server/app/Models/UserQuizAnswer.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserQuizAnswer extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'video_id',
+        'question',
+        'selected',
+        'correct',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
