6ad850eb3382b840bfb7b9e3969bce89d32591ea
GHADY-MATTA <ghady5255@gmail.com>
2025-05-19 09:56:27 +0300

    updated StoreUserYouTubeLogRequest

--- server/app/Http/Requests/StoreUserYouTubeLogRequest.php
+++ server/app/Http/Requests/StoreUserYouTubeLogRequest.php
@@ -11,7 +11,7 @@ class StoreUserYouTubeLogRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,6 +22,9 @@ class StoreUserYouTubeLogRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'user_id' => 'required|integer|exists:users,id',
+            'video_url' => 'required|url',
+            'youtube_video_id' => 'nullable|string',
             //
         ];
     }
