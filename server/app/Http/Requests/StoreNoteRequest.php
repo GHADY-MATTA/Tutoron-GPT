d0b968ff7e6d51e9e262a79e9357948524d88522
GHADY-MATTA <ghady5255@gmail.com>
2025-05-19 09:33:33 +0300

    Updated StoreNoteRequest

--- server/app/Http/Requests/StoreNoteRequest.php
+++ server/app/Http/Requests/StoreNoteRequest.php
@@ -11,7 +11,7 @@ class StoreNoteRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,6 +22,8 @@ class StoreNoteRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'user_id' => 'required|exists:users,id',
+            'content' => 'required|string',
             //
         ];
     }
