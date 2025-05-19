0f190322124721cbc9683024a272e7d9c8dd7e53
GHADY-MATTA <ghady5255@gmail.com>
2025-05-19 09:27:42 +0300

    Updated StoreTranscriptRequest

--- server/app/Http/Requests/StoreTranscriptRequest.php
+++ server/app/Http/Requests/StoreTranscriptRequest.php
@@ -11,7 +11,7 @@ class StoreTranscriptRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,6 +22,9 @@ class StoreTranscriptRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'video_id' => 'required|string',
+            'title' => 'required|string',
+            'transcript_raw' => 'required|string',
             //
         ];
     }
