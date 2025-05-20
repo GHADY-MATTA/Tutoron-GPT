54fd7177b3005260086d50d0248187ca07868900
GHADY-MATTA <ghady5255@gmail.com>
2025-05-20 17:35:55 +0300

    fixed tutoron

--- client/vite-project/src/pages/tutoron.jsx
+++ client/vite-project/src/pages/tutoron.jsx
@@ -102,7 +102,7 @@ function Tutoron() {
               </div>
               <p className="text-sm text-[var(--color-text-light)] truncate">List of books to read this summer</p>
               <div className="flex mt-2 gap-1 flex-wrap">
-                
+                <span className="text-xs tag px-2 py-1 rounded-full">personal</span>
                 
               </div>
             </li>
