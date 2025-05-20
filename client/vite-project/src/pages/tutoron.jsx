a626b1648ce0d78dc2805cc9a92ea353e6aba122
GHADY-MATTA <ghady5255@gmail.com>
2025-05-20 17:28:41 +0300

    fixed tutoron

---
+++ client/vite-project/src/pages/tutoron.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+function Tutoron() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return <div>Tutoron UI</div>;
+}
+
+export default Tutoron;
